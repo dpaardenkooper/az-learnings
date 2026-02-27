66b6b190541bb3177a4724a30858439051e2af26
d.paardenkooper <d.paardenkooper@fullstaq.com>
2026-02-27 16:24:00 +0100

    ai-cp Lab9

--- AI_Specific/Develop_Computer_Vision_Solutions_in_Azure/Labs/mslearn-ai-visio/Labfiles/09 dalle-client/python/dalle-client.py
+++ AI_Specific/Develop_Computer_Vision_Solutions_in_Azure/Labs/mslearn-ai-visio/Labfiles/09 dalle-client/python/dalle-client.py
@@ -1,43 +1,41 @@
-import os
 import json
+import os
 
+import requests
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 # Add references
 from dotenv import load_dotenv
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from openai import AzureOpenAI
-import requests
 
 
-
-def main(): 
+def main():
 
     # Clear the console
-    os.system('cls' if os.name=='nt' else 'clear')
-        
-    try: 
-    
-        # Get configuration settings 
+    os.system("cls" if os.name == "nt" else "clear")
+
+    try:
+
+        # Get configuration settings
         load_dotenv()
         endpoint = os.getenv("ENDPOINT")
-        model_deployment =  os.getenv("MODEL_DEPLOYMENT")
+        model_deployment = os.getenv("MODEL_DEPLOYMENT")
         api_version = os.getenv("API_VERSION")
-        
+
         # Initialize the client
         token_provider = get_bearer_token_provider(
-            DefaultAzureCredential(exclude_environment_credential=True,
-                exclude_managed_identity_credential=True), 
-            "https://cognitiveservices.azure.com/.default"
+            DefaultAzureCredential(
+                exclude_environment_credential=True,
+                exclude_managed_identity_credential=True,
+            ),
+            "https://cognitiveservices.azure.com/.default",
         )
-            
+
         client = AzureOpenAI(
             api_version=api_version,
             azure_endpoint=endpoint,
-            azure_ad_token_provider=token_provider
+            azure_ad_token_provider=token_provider,
         )
-        
-        
 
-         
         img_no = 0
         # Loop until the user types 'quit'
         while True:
@@ -48,30 +46,27 @@ def main():
             if len(input_text) == 0:
                 print("Please enter a prompt.")
                 continue
-            
+
             # Generate an image
             result = client.images.generate(
-                model=model_deployment,
-                prompt=input_text,
-                n=1
+                model=model_deployment, prompt=input_text, n=1
             )
 
             json_response = json.loads(result.model_dump_json())
-            image_url = json_response["data"][0]["url"] 
-            
+            image_url = json_response["data"][0]["url"]
 
             # save the image
             img_no += 1
             file_name = f"image_{img_no}.png"
-            save_image (image_url, file_name)
-
+            save_image(image_url, file_name)
 
     except Exception as ex:
         print(ex)
 
-def save_image (image_url, file_name):
+
+def save_image(image_url, file_name):
     # Set the directory for the stored image
-    image_dir = os.path.join(os.getcwd(), 'images')
+    image_dir = os.path.join(os.getcwd(), "images")
 
     # If the directory doesn't exist, create it
     if not os.path.isdir(image_dir):
@@ -84,8 +79,8 @@ def save_image (image_url, file_name):
     generated_image = requests.get(image_url).content  # download the image
     with open(image_path, "wb") as image_file:
         image_file.write(generated_image)
-    print (f"Image saved as {image_path}")
+    print(f"Image saved as {image_path}")
 
 
-if __name__ == '__main__': 
+if __name__ == "__main__":
     main()
