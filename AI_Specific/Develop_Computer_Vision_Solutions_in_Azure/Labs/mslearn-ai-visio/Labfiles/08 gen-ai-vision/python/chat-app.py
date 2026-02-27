76bf81ff271f6f6688765e973d869998ff0560bf
d.paardenkooper <d.paardenkooper@fullstaq.com>
2026-02-27 16:21:32 +0100

    ai-cp Lab8

--- AI_Specific/Develop_Computer_Vision_Solutions_in_Azure/Labs/mslearn-ai-visio/Labfiles/08 gen-ai-vision/python/chat-app.py
+++ AI_Specific/Develop_Computer_Vision_Solutions_in_Azure/Labs/mslearn-ai-visio/Labfiles/08 gen-ai-vision/python/chat-app.py
@@ -1,44 +1,38 @@
-import os
-from urllib.request import urlopen, Request
 import base64
+import os
 from pathlib import Path
-from dotenv import load_dotenv
+from urllib.request import Request, urlopen
 
+from azure.ai.projects import AIProjectClient
 # Add references
 from azure.identity import DefaultAzureCredential
-from azure.ai.projects import AIProjectClient
+from dotenv import load_dotenv
 from openai import AzureOpenAI
 
 
-def main(): 
+def main():
 
     # Clear the console
-    os.system('cls' if os.name=='nt' else 'clear')
-        
-    try: 
-    
-        # Get configuration settings 
+    os.system("cls" if os.name == "nt" else "clear")
+
+    try:
+
+        # Get configuration settings
         load_dotenv()
         project_endpoint = os.getenv("PROJECT_CONNECTION")
-        model_deployment =  os.getenv("MODEL_DEPLOYMENT")
-
+        model_deployment = os.getenv("MODEL_DEPLOYMENT")
 
         # Initialize the project client
-        project_client = AIProjectClient(            
-                credential=DefaultAzureCredential(
-                    exclude_environment_credential=True,
-                    exclude_managed_identity_credential=True
-                ),
-                endpoint=project_endpoint,
-            )
-
-        
+        project_client = AIProjectClient(
+            credential=DefaultAzureCredential(
+                exclude_environment_credential=True,
+                exclude_managed_identity_credential=True,
+            ),
+            endpoint=project_endpoint,
+        )
 
         # Get a chat client
         openai_client = project_client.get_openai_client(api_version="2024-10-21")
-        
-
-
 
         # Initialize prompts
         system_message = "You are an AI assistant in a grocery store that sells fruit. You provide detailed answers to questions about produce."
@@ -46,14 +40,15 @@ def main():
 
         # Loop until the user types 'quit'
         while True:
-            prompt = input("\nAsk a question about the image\n(or type 'quit' to exit)\n")
+            prompt = input(
+                "\nAsk a question about the image\n(or type 'quit' to exit)\n"
+            )
             if prompt.lower() == "quit":
                 break
             elif len(prompt) == 0:
-                    print("Please enter a question.\n")
+                print("Please enter a question.\n")
             else:
                 print("Getting a response ...\n")
-
 
                 # Get a response to image input
                 image_url = "https://github.com/MicrosoftLearning/mslearn-ai-vision/raw/refs/heads/main/Labfiles/gen-ai-vision/orange.jpeg"
@@ -66,19 +61,20 @@ def main():
                     model=model_deployment,
                     messages=[
                         {"role": "system", "content": system_message},
-                        { "role": "user", "content": [  
-                            { "type": "text", "text": prompt},
-                            { "type": "image_url", "image_url": {"url": data_url}}
-                        ] } 
-                    ]
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "text", "text": prompt},
+                                {"type": "image_url", "image_url": {"url": data_url}},
+                            ],
+                        },
+                    ],
                 )
                 print(response.choices[0].message.content)
-                    
-
 
     except Exception as ex:
         print(ex)
 
 
-if __name__ == '__main__': 
+if __name__ == "__main__":
     main()
