428cbe5397350d9499261914a0b9243874d0b342
d.paardenkooper <d.paardenkooper@fullstaq.com>
2026-02-27 17:00:19 +0100

    ai-extrat Lab5

--- AI_Specific/Develop_AI-powered_information_extraction_solutions_in_Azure/Labs/ai-info-extraction/Labfiles/05 knowledge/python/search-app.py
+++ AI_Specific/Develop_AI-powered_information_extraction_solutions_in_Azure/Labs/ai-info-extraction/Labfiles/05 knowledge/python/search-app.py
@@ -1,26 +1,29 @@
-from dotenv import load_dotenv
 import os
+
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
+from dotenv import load_dotenv
 
 
 def main():
 
     # Clear the console
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
     try:
 
         # Get config settings
         load_dotenv()
-        search_endpoint = os.getenv('SEARCH_ENDPOINT')
-        query_key = os.getenv('QUERY_KEY')
-        index = os.getenv('INDEX_NAME')
+        search_endpoint = os.getenv("SEARCH_ENDPOINT")
+        query_key = os.getenv("QUERY_KEY")
+        index = os.getenv("INDEX_NAME")
 
         # Get a search client
-        search_client = SearchClient(search_endpoint, index, AzureKeyCredential(query_key))
+        search_client = SearchClient(
+            search_endpoint, index, AzureKeyCredential(query_key)
+        )
 
-         # Loop until the user types 'quit'
+        # Loop until the user types 'quit'
         while True:
             # Get query text
             query_text = input("Enter a query (or type 'quit' to exit): ")
@@ -31,14 +34,14 @@ def main():
                 continue
 
             # Clear the console
-            os.system('cls' if os.name=='nt' else 'clear')
-            
+            os.system("cls" if os.name == "nt" else "clear")
+
             # Search the index
             found_documents = search_client.search(
                 search_text=query_text,
                 select=["metadata_storage_name", "locations", "people", "keyphrases"],
                 order_by=["metadata_storage_name"],
-                include_total_count=True
+                include_total_count=True,
             )
 
             # Parse the results
@@ -59,6 +62,5 @@ def main():
         print(ex)
 
 
-
 if __name__ == "__main__":
-    main()        
+    main()
