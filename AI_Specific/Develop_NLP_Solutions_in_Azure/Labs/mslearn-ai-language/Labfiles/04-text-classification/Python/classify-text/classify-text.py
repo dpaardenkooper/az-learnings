a20b5f4d3332741a81b3ee4257d5da1991b09bd0
Peter Kuiper <peter@2force.nl>
2026-03-02 17:12:20 +0100

    feat: updated 04-text-classification

--- AI_Specific/Develop_NLP_Solutions_in_Azure/Labs/mslearn-ai-language/Labfiles/04-text-classification/Python/classify-text/classify-text.py
+++ AI_Specific/Develop_NLP_Solutions_in_Azure/Labs/mslearn-ai-language/Labfiles/04-text-classification/Python/classify-text/classify-text.py
@@ -10,46 +10,49 @@ def main():
     try:
         # Get Configuration Settings
         load_dotenv()
-        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
-        ai_key = os.getenv('AI_SERVICE_KEY')
-        project_name = os.getenv('PROJECT')
-        deployment_name = os.getenv('DEPLOYMENT')
+        ai_endpoint = os.getenv("AI_SERVICE_ENDPOINT")
+        ai_key = os.getenv("AI_SERVICE_KEY")
+        project_name = os.getenv("PROJECT")
+        deployment_name = os.getenv("DEPLOYMENT")
 
         # Create client using endpoint and key
         credential = AzureKeyCredential(ai_key)
         ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
-
         # Read each text file in the articles folder
         batchedDocuments = []
-        articles_folder = 'articles'
+        articles_folder = "articles"
         files = os.listdir(articles_folder)
         for file_name in files:
             # Read the file contents
-            text = open(os.path.join(articles_folder, file_name), encoding='utf8').read()
+            text = open(
+                os.path.join(articles_folder, file_name), encoding="utf8"
+            ).read()
             batchedDocuments.append(text)
 
         # Get Classifications
         operation = ai_client.begin_single_label_classify(
-        batchedDocuments,
-        project_name=project_name,
-        deployment_name=deployment_name
+            batchedDocuments, project_name=project_name, deployment_name=deployment_name
         )
 
         document_results = operation.result()
 
         for doc, classification_result in zip(files, document_results):
-        if classification_result.kind == "CustomDocumentClassification":
-            classification = classification_result.classifications[0]
-            print("{} was classified as '{}' with confidence score {}.".format(
-                doc, classification.category, classification.confidence_score)
-            )
-        elif classification_result.is_error is True:
-            print("{} has an error with code '{}' and message '{}'".format(
-                doc, classification_result.error.code, classification_result.error.message)
-            )
-
-
+            if classification_result.kind == "CustomDocumentClassification":
+                classification = classification_result.classifications[0]
+                print(
+                    "{} was classified as '{}' with confidence score {}.".format(
+                        doc, classification.category, classification.confidence_score
+                    )
+                )
+            elif classification_result.is_error is True:
+                print(
+                    "{} has an error with code '{}' and message '{}'".format(
+                        doc,
+                        classification_result.error.code,
+                        classification_result.error.message,
+                    )
+                )
 
     except Exception as ex:
         print(ex)
